e0458deffddbbc0220ed993fc5a88ed59e42d24a
Frostetsky <nesterenko-nikita@bk.ru>
2020-09-15 12:55:50 +0300

    6. Перевернуть связанный список [#342965]

---
+++ src/main/java/ru/job4j/chapter_001/list/ForwardLinked.java
@@ -0,0 +1,67 @@
+package ru.job4j.chapter_001.list;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ForwardLinked<T> implements Iterable<T> {
+    private Node<T> head;
+    private Node<T> newHead;
+
+    public void add(T value) {
+        Node<T> node = new Node<T>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void revert() {
+        Node reversedPart = null;
+        Node current = head;
+        while (current != null) {
+            Node next = current.next;
+            current.next = reversedPart;
+            reversedPart = current;
+            current = next;
+        }
+        head = reversedPart;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
