2b84b063ee0af1cc1a784787b33235e9e52e85bc
Frostetsky <nesterenko-nikita@bk.ru>
2020-09-18 07:59:05 +0300

    0.2. FileInputStream [#343020]

---
+++ src/main/java/ru/job4j/chapter_002/java_IO/fileinputstream/EvenNumberFile.java
@@ -0,0 +1,30 @@
+package ru.job4j.chapter_002.java_IO.fileinputstream;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in =
+                     new FileInputStream("C:\\Users\\Nikita\\IdeaProjects\\job4j_design\\" +
+                                                "src\\main\\java\\ru\\job4j\\chapter_002\\java_IO\\" +
+                                                "fileinputstream\\even.txt"))
+        {
+            StringBuilder builder = new StringBuilder();
+            int read;
+            while (( read = in.read()) != -1) {
+                builder.append((char) read);
+            }
+
+            String[] lines = builder.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                if (Integer.valueOf(line) % 2 == 0) {
+                    System.out.println(true);
+                } else {
+                    System.out.println(false);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
