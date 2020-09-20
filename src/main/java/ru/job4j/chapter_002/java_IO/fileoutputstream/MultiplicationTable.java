07e0b81d639252fda99fec386eba874d28a4e785
Frostetsky <nesterenko-nikita@bk.ru>
2020-09-20 09:32:02 +0300

    0.1. FileOutputStream. [#343010]

--- src/main/java/ru/job4j/chapter_002/java_IO/fileoutputstream/MultiplicationTable.java
+++ src/main/java/ru/job4j/chapter_002/java_IO/fileoutputstream/MultiplicationTable.java
@@ -5,9 +5,7 @@ import java.io.FileOutputStream;
 public class MultiplicationTable {
     public static void main(String[] args) {
         try (FileOutputStream out =
-                     new FileOutputStream("C:\\Users\\Nikita\\IdeaProjects\\job4j_design\\" +
-                                                "src\\main\\java\\ru\\job4j\\chapter_002\\java_IO\\" +
-                                                "fileoutputstream\\multiplicationtable.txt")) {
+                     new FileOutputStream("./src/main/java/ru/job4j/chapter_002/java_IO/fileoutputstream/multiplicationtable.txt")) {
             for (int i = 1; i <= 10; i++) {
                 for (int j = 1; j <= 10; j++) {
                     out.write(Integer.toString(i * j).getBytes());
