15f358648691e7e00a6be8b91983de883e1f7b29
Frostetsky <nesterenko-nikita@bk.ru>
2020-09-21 14:54:54 +0300

    2. Анализ доступности сервера. [#343017]

--- src/main/java/ru/job4j/chapter_002/java_IO/serveranalysis/Analizy.java
+++ src/main/java/ru/job4j/chapter_002/java_IO/serveranalysis/Analizy.java
@@ -1,16 +1,14 @@
 package ru.job4j.chapter_002.java_IO.serveranalysis;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.*;
 
 public class Analizy {
 
     private static Map<String, Integer> log = new LinkedHashMap<>();
+    private static List<String> result = new ArrayList<>();
 
-    public void unavailable(String source, String target) {
+    public void unavailable(String source, String target) throws IOException {
         StringJoiner out = new StringJoiner(System.lineSeparator());
         try (BufferedReader read = new BufferedReader(new FileReader(source))) {
             read.lines().forEach(out::add);
@@ -22,35 +20,45 @@ public class Analizy {
                 }
                 log.put(elementmaps[1], Integer.valueOf(elementmaps[0]));
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
-            List<Integer> times  = new ArrayList<>(log.values());
+            read.close();
+            List<Integer> times = new ArrayList<>(log.values());
             int index = 0;
             for (Map.Entry<String, Integer> pair : log.entrySet()) {
                 Integer number = pair.getValue();
                 String time = pair.getKey();
-                if ( index != 0) {
+                if (index != 0) {
                     if ((number == 500 || number == 400) && (times.get(index - 1) == 200 || times.get(index - 1) == 300)) {
-                        writer.write(time + " -- ");
+                        result.add(time);
                     }
                 }
-                if ( index != 0) {
+                if (index != 0) {
                     if ((times.get(index - 1) == 500 || times.get(index - 1) == 400) && (number == 200 || number == 300)) {
-                        writer.write(time);
-                        writer.write(System.lineSeparator());
+                        result.add(time);
                     }
                 }
                 index++;
             }
-        } catch (Exception e) {
+        } catch (IncorrectInitialDataException e) {
             e.printStackTrace();
         }
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(target));
+        int index = 0;
+        for (String time : result) {
+            if (index == 0) {
+                writer.write(time + " ");
+                index++;
+            } else if (index == 1) {
+                writer.write(time);
+                writer.write(System.lineSeparator());
+                index = 0;
+            }
+        }
+        writer.close();
     }
 
 
-    public static void main(String[] args) {
+    public static void main (String[]args) throws IOException {
         String source = "./src/main/java/ru/job4j/chapter_002/java_IO/serveranalysis/server.log";
         String target = "./src/main/java/ru/job4j/chapter_002/java_IO/serveranalysis/result.log";
         new Analizy().unavailable(source, target);
