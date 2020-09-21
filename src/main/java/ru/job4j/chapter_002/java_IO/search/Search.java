cd12d45c7f7af3ede0550614955a6b8edbc985e3
Frostetsky <nesterenko-nikita@bk.ru>
2020-09-21 13:18:46 +0300

    4.1. Сканирование файловой системы. [#343007]

--- src/main/java/ru/job4j/chapter_002/java_IO/search/Search.java
+++ src/main/java/ru/job4j/chapter_002/java_IO/search/Search.java
@@ -1,27 +1,21 @@
 package ru.job4j.chapter_002.java_IO.search;
 
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
 import java.io.IOException;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            throw new IllegalArgumentException();
-        }
-        File file = new File(args[0]);
-        String[] enlargement = new String[args.length - 1];
-        for (int i = 1; i < args.length; i++) {
-            enlargement[i - 1] = args[i];
-        }
-        search(file, enlargement).forEach(System.out::println);
+        Path start = Paths.get("./src");
+        search(start, "txt").forEach(System.out::println);
     }
 
-    public static List<File> search(File root, String[] ext) {
-        List<File> files = (List<File>) FileUtils.listFiles(root, ext, true);
-        return files;
+
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
     }
 }
---
+++ src/main/java/ru/job4j/chapter_002/java_IO/search/SearchFiles.java
@@ -0,0 +1,47 @@
+package ru.job4j.chapter_002.java_IO.search;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFiles implements FileVisitor<Path> {
+
+    private List<Path> result = new ArrayList<>();
+    private Predicate<Path> format;
+
+    public SearchFiles(Predicate<Path> format) {
+        this.format = format;
+    }
+
+    public List<Path> getPaths() {
+        return result;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (format.test(file.getFileName())) {
+            result.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+}
