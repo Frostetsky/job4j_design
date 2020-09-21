70c5e4692680b077fdd88b6dc4d5adcbbae5b10d
Frostetsky <nesterenko-nikita@bk.ru>
2020-09-21 08:28:10 +0300

    5.1. Именованные аргументы [#343014]

---
+++ src/main/java/ru/job4j/chapter_002/java_IO/argsparam/ArgsName.java
@@ -0,0 +1,39 @@
+package ru.job4j.chapter_002.java_IO.argsparam;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        for (String arg : args) {
+            String clearline = arg.substring(1);
+            if (arg.charAt(0) != 45) {
+                throw new IllegalArgumentException();
+            }
+            String[] element = clearline.split("=");
+            System.out.println(Arrays.toString(element));
+            if (element.length > 2) {
+                throw new IllegalArgumentException();
+            }
+            values.put(element[0],element[1]);
+        }
+
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+}
---
+++ src/test/java/ru/job4j/chapter_002/java_IO/argsparam/ArgsNameTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.chapter_002.java_IO.argsparam;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class ArgsNameTest {
+
+    @Test
+    public void whenGetFirst() {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        assertThat(jvm.get("Xmx"), is("512"));
+    }
+
+    @Test
+    public void whenGetFirstReorder() {
+        ArgsName jvm = ArgsName.of(new String[] {"-encoding=UTF-8", "-Xmx=512"});
+        assertThat(jvm.get("Xmx"), is("512"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenGetNotExist() {
+        ArgsName jvm = ArgsName.of(new String[] {});
+        jvm.get("Xmx");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNoHyphen() {
+        ArgsName xmx = ArgsName.of(new String[] {"Xmx=512", "-encoding=UTF-8"});
+        xmx.get("Xmx");
+    }
+}
