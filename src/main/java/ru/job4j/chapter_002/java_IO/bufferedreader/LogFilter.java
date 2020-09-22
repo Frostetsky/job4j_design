238a73f8a881ffea2b08f68d30556fd5e9414912
Frostetsky <nesterenko-nikita@bk.ru>
2020-09-22 16:23:15 +0300

    0.3. BufferedReader. [#343011]

--- src/main/java/ru/job4j/chapter_002/java_IO/bufferedreader/LogFilter.java
+++ src/main/java/ru/job4j/chapter_002/java_IO/bufferedreader/LogFilter.java
@@ -3,23 +3,21 @@ package ru.job4j.chapter_002.java_IO.bufferedreader;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class LogFilter {
     public static List<String> filter(String file) {
         List<String> result = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-           List<String> tempresult = reader.lines().collect(Collectors.toList());
-           for (String element : tempresult) {
-               String[] strings = element.split(" ");
-               if (Integer.valueOf(strings[strings.length - 2]) == 404) {
-                   result.add(element);
-               }
-           }
+           reader.lines().filter(LogFilter::filtration).forEach(result::add);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return result;
+    }
+
+    private static boolean filtration(String element) {
+        String[] strings = element.split(" ");
+        return (Integer.valueOf(strings[strings.length - 2]) == 404);
     }
 
     public static void save(List<String> log, String file) {
