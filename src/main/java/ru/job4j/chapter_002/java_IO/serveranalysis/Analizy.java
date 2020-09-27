8696ce3e5d072d5a23ce89700593cc1f531f8c37
Frostetsky <nesterenko-nikita@bk.ru>
2020-09-27 14:54:50 +0300

    2. Анализ доступности сервера. [#343017]

--- src/main/java/ru/job4j/chapter_002/java_IO/serveranalysis/Analizy.java
+++ src/main/java/ru/job4j/chapter_002/java_IO/serveranalysis/Analizy.java
@@ -5,25 +5,25 @@ import java.util.*;
 
 public class Analizy {
 
-    private static List<String> result = new ArrayList<>();
+    private static final List<String> result = new ArrayList<>();
 
     public void unavailable(String source, String target) {
         try (BufferedReader read = new BufferedReader(new FileReader(source))) {
             String line;
+            int index = -1;
             List<String> times = new ArrayList<>();
             while ((line = read.readLine()) != null) {
                 times.add(line);
-            }
-            for (int index = 0; index < times.size(); index++) {
+                index++;
                 if (index == 0) {
                     continue;
                 }
-                Integer index1 = Integer.valueOf(times.get(index).split(" ")[0]);
-                Integer index2 = Integer.valueOf(times.get(index - 1).split(" ")[0]);
-                if ((index1 == 500 || index1 == 400) && (index2 == 200 || index2 == 300)) {
+                Integer index1 = Integer.valueOf(line.split(" ")[0]);
+                Integer lastindex = Integer.valueOf(times.get(index - 1).split(" ")[0]);
+                if ((index1 == 500 || index1 == 400) && (lastindex == 200 || lastindex == 300)) {
                     result.add(times.get(index).split(" ")[1]);
                 }
-                if ((index2 == 500 || index2 == 400) && (index1 == 200 || index1 == 300)) {
+                if ((lastindex == 500 || lastindex == 400) && (index1 == 200 || index1 == 300)) {
                     result.add(times.get(index).split(" ")[1]);
                 }
             }
@@ -47,7 +47,7 @@ public class Analizy {
         }
     }
 
-    public static void main (String[]args) throws IOException {
+    public static void main (String[]args) {
         String source = "./src/main/java/ru/job4j/chapter_002/java_IO/serveranalysis/server.log";
         String target = "./src/main/java/ru/job4j/chapter_002/java_IO/serveranalysis/result.log";
         new Analizy().unavailable(source, target);
