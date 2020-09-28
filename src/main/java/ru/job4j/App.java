297fb40de9a84c0b0cbfe697f1ca788c57b5e68e
Frostetsky <nesterenko-nikita@bk.ru>
2020-09-28 23:11:14 +0300

    Return Default

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -9,17 +9,6 @@ import java.util.*;
 public class App 
 {
     public static void main( String[] args ) {
-        List<Dog> dogs = new ArrayList<>();
-        dogs.add(new Dog());
-        List<Animal> animals = new ArrayList<>();
-        animals.add(new Animal());
-        List<Object> objects = new ArrayList<>();
-        objects.add(new Object());
-        App.filter(objects);
-        App.filter(animals);
-    }
 
-    private static void filter(List<? super Animal> list) {
-        System.out.println(list);
     }
 }
