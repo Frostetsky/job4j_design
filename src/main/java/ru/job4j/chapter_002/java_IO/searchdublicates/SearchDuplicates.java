5629950f596190737831cd81c2c404d78e2d1d11
Frostetsky <nesterenko-nikita@bk.ru>
2020-11-16 05:48:51 +0300

    4.2. Поиск дубликатов [#343015]

--- src/main/java/ru/job4j/chapter_002/java_IO/searchdublicates/SearchDuplicates.java
+++ src/main/java/ru/job4j/chapter_002/java_IO/searchdublicates/SearchDuplicates.java
@@ -1,12 +1,29 @@
 package ru.job4j.chapter_002.java_IO.searchdublicates;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
 import java.util.*;
 
 public class SearchDuplicates {
-    public static Set<String> searchDuplicate(File catalog) {
-        // TODO сделать через HashSet, проверять условие дубликата при добавлении в Set
-        return null;
+    private static Map<Long, File> duplicates = new HashMap<>();
+    private static Set<File> result = new HashSet<>();
+
+    public static Set<File> searchDuplicate(File catalog) {
+        for (File file : Objects.requireNonNull(catalog.listFiles())) {
+            if (file.isDirectory()) {
+                searchDuplicate(file);
+            } else {
+                long sizeFile = FileUtils.sizeOf(file);
+                if (duplicates.containsKey(sizeFile) && duplicates.get(sizeFile).getName().equals(file.getName())) {
+                    result.add(file);
+                    result.add(duplicates.get(sizeFile));
+                } else {
+                    duplicates.put(sizeFile, file);
+                }
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
